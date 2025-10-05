b4c1a903391a7e1a56b1cdb0120229af85c00a55
Alaa Mardini <a-mardini@aeu.ac.sy>
2025-10-05 10:41:10 +0300

    fix start script for render

--- backend/server.js
+++ backend/server.js
@@ -43,8 +43,7 @@ app.use(cors({
   origin: [
     'https://ilo-aiu.onrender.com',  
     'http://localhost:5000',        
-    'http://localhost:5173',         
-    'http://127.0.0.1:5173',],
+    'http://localhost:5173'],
   credentials: true
 }));
 
--- lib/constants/config.dart
+++ lib/constants/config.dart
@@ -1,4 +1,3 @@
-// config.dart
 const String baseUrl = String.fromEnvironment(
   'API_URL',
   defaultValue: 'http://localhost:5000',
